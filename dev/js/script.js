f615aa691353e3901d0e76c0dd0d0e047941b3b9
Badaboom1995 <kosplay@mail.ru>
2016-03-18 18:11:22 +0300

    merge complete

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,3 +1,9 @@
- $(window).resize(function(){
-window.setTimeout('location.reload()', 100);
-});
+ function initialize() {     
+	var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+	var myOptions = {
+		zoom: 8,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map"), myOptions); 
+}
